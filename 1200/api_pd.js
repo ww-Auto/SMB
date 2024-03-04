3bb8033ea24d37c33469c9380d31b346c5d15ff9
ww-Auto <96040657+ww-Auto@users.noreply.github.com>
2024-03-04 17:00:12 +0900

    [SMB-JH] Update api_pd.js

--- 1200/api_pd.js
+++ 1200/api_pd.js
@@ -26,45 +26,54 @@ if (!dir) fs.mkdirSync("../result/PD_API_Result");
 
 // Loading Search API Data
 // var filePath = "Y:/smb/2024smb/sit/data/2024-01-23/Search_API_Result/"
-var filePath = "../outputs/api/search";
-
-fs.readdir(filePath, function (err, file) {
-    for (var a = 0; a < file.length; a++) {
-
-
-        fs.readFile(filePath + '/' + file[a], 'utf8', (error, jsonFile) => {
-            var jsonData = JSON.parse(jsonFile);
-            var promiseList = new Array();
-
-            for (var b = 0; b < jsonData.length; b++) {
-                var sSKU = jsonData[b].modelCode;
-                var sitecode = jsonData[0].sitecode;
-                var api_temp = getPDAPI(sSKU, sitecode);
-                if (api_temp != "") promiseList.push(api_temp);
-            }
-
-            Promise.all(promiseList).then((data) => {
-                data = data.reduce(function (acc, cur) {
-                    return acc.concat(cur);
-                });
-                data = data.reduce(function (acc, cur) {
-                    return acc.concat(cur);
-                });
-
-                const result = JSON.stringify(data, null, 2);
-                var cur_sc = data[0].Site;
-
-                // Creat Json File
-                fs.writeFileSync('../result/PD_API_Result/PD_API_' + cur_sc + '.json', result);
-                console.log("PD_API_" + cur_sc + "_JSON File Saved!");
-
-                // Creat Excel File
-                const workBook = xlsx.utils.book_new();
-                var workSheet = xlsx.utils.json_to_sheet(data);
-                xlsx.utils.book_append_sheet(workBook, workSheet, cur_sc);
-                xlsx.writeFile(workBook, "../result/PD_API_Result/PD_API_" + cur_sc + '.xlsx');
-
-            });
-        });
+var filePath = "D:\\SMB\\outputs\\api\\search"
+    
+fs.readdir(filePath, function (err, files) {
+    if (err) {
+        console.log("Error reading directory", err);
+        return;
     }
+
+    files.forEach(file => {
+        if (file.endsWith('.json')) {
+            fs.readFile(`${filePath}/${file}`, 'utf8', (error, jsonFile) => {
+                if (error) {
+                    console.log(`Error reading file ${file}`, error);
+                    return;
+                }
+                var jsonData = JSON.parse(jsonFile);
+                var promiseList = new Array();
+
+                for (var b = 0; b < jsonData.length; b++) {
+                    var sSKU = jsonData[b].modelCode;
+                    var sitecode = jsonData[0].sitecode;
+                    var api_temp = getPDAPI(sSKU, sitecode);
+                    if (api_temp != "") promiseList.push(api_temp);
+                }
+
+                Promise.all(promiseList).then((data) => {
+                    data = data.reduce(function (acc, cur) {
+                        return acc.concat(cur);
+                    });
+                    data = data.reduce(function (acc, cur) {
+                        return acc.concat(cur);
+                    });
+
+                    const result = JSON.stringify(data, null, 2);
+                    var cur_sc = data[0].Site;
+
+                    // Creat Json File
+                    fs.writeFileSync('../result/PD_API_Result/PD_API_' + cur_sc + '.json', result);
+                    console.log("PD_API_" + cur_sc + "_JSON File Saved!");
+
+                    // Creat Excel File
+                    const workBook = xlsx.utils.book_new();
+                    var workSheet = xlsx.utils.json_to_sheet(data);
+                    xlsx.utils.book_append_sheet(workBook, workSheet, cur_sc);
+                    xlsx.writeFile(workBook, "../result/PD_API_Result/PD_API_" + cur_sc + '.xlsx');
+
+                });
+            });
+        }
+    });
 });
